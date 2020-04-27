6978f5e78a437d557d5f2fbadd4947e18477559b
DmEremin <dm.eremin271090@gmail.com>
2020-04-28 00:30:50 +0300

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+URL = 'https://www.fl.ru/projects/'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
+           'accept': '*/*'}
+HOST = 'https://www.fl.ru'
+
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def find_price(html):
+    start = html.find('&nbsp')
+    end = html.rfind('&nbsp')
+    count = html[start - 4: start].replace('>', '').replace('a', '').strip()
+
+    if start != end:
+        count += html[end - 3: end]
+
+    if 'euro' in html:
+        currency = 'EUR'
+    elif '$' in html:
+        currency = 'USD'
+    elif '₽' in html:
+        currency = 'RUB'
+    else:
+        count = None
+        currency = None
+
+    return {'count': count, 'currency': currency}
+
+
+def get_content(html):
+    soup = BS(html, 'html.parser')
+    items = soup.find_all('div', class_='b-post_padbot_15')
+
+    tasks = []
+    for item in items:
+        tasks.append({
+            'title': item.find('a', class_='b-post__link').get_text(),
+            'link': HOST + item.find('a', class_='b-post__link').get('href'),
+            'price': find_price(str(item.find('script')))
+        })
+    print(tasks)
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print('Error')
+
+
+parse()
