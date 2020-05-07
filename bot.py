bd564fdbb105c46207a74438f70468a50248502a
Еремин Д <eremin@AN-EREMIN.DLX>
2020-05-07 11:53:46 +0300

    A little changes

--- bot.py
+++ bot.py
@@ -1,23 +1,12 @@
 import telebot
 import config
-import json
+import tools
 from pprint import pprint
-
-
-def js_write(data):
-    with open("options.json", "w") as write_file:
-        json.dump(data, write_file)
-
-
-def js_read():
-    with open("options.json", "r") as read_file:
-        return json.load(read_file)
-
 
 bot = telebot.TeleBot(config.TOKEN)
 
 try:
-    options = js_read()
+    options = tools.js_read()
 except FileNotFoundError:
     options = {}
 
@@ -25,32 +14,30 @@ except FileNotFoundError:
 @bot.message_handler(content_types=['text'])
 def dialog(message):
     user_id = str(message.from_user.id)
-    message_id = message.chat.id
-    # user_options = options.get(user_id)
 
     if message.text == 'parse':
         if options.get(user_id):  # if user is old
             options[user_id]['status'] = 'pending'
             print('old')
         else:
-            options[user_id] = {'status': 'pending', 'chat_id': message_id}
+            options[user_id] = {'status': 'pending', 'history': {}}
             print('new')
 
-        bot.send_message(message_id, 'Введите ключевое слово')
+        bot.send_message(user_id, 'Введите ключевое слово')
         pprint(options)
 
     elif options.get(user_id) and options[user_id]['status'] == 'pending':
         options[user_id]['current_keyword'] = message.text
         options[user_id]['status'] = 'turned'
-        js_write(options)
+        tools.js_write(options)
         pprint(options)
 
     elif message.text == 'dump':
         pprint(options)
-        bot.send_message(message_id, 'Изи дамп')
+        bot.send_message(user_id, 'Изи дамп')
 
     else:
-        bot.send_message(message_id, 'Не понимаю :(')
+        bot.send_message(user_id, 'Не понимаю :(')
         pprint(options)
 
 
